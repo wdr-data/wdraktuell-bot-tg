a81fcf3279a47091615c8956b43cf6fe261344ea
ChristianJoe <cjoe@gmx.de>
2020-03-12 16:39:17 +0100

    :bento: Use dedicated datapolicy faqs

--- handlers/dataPolicy.js
+++ handlers/dataPolicy.js
@@ -1,10 +1,10 @@
 import getFaq from '../lib/faq';
 
 export default async (ctx) => {
-    let faq = await getFaq('datenschutz');
+    let faq = await getFaq('datenschutz_general');
     await ctx.replyFullNewsBase(faq);
-    faq = await getFaq('datenschutz2');
+    faq = await getFaq('datenschutz_tracking');
     await ctx.replyFullNewsBase(faq);
-    faq = await getFaq('datenschutz3');
+    faq = await getFaq('datenschutz_policies');
     await ctx.replyFullNewsBase(faq);
 };
