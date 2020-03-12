a73c21b5d5dfce864ed2ceec9c03c02c53c01fab
P3nny <p3nny@hacking.studio>
2020-03-12 14:37:30 +0100

    💩  Handle 'datenschutz' intent

--- handlers/faq.js
+++ handlers/faq.js
@@ -4,3 +4,10 @@ export const handleFaq = async (ctx) => {
     const faq = await getFaq(ctx.data.faq);
     await ctx.replyFullNewsBase(faq);
 };
+
+export const handleActionFaq = (slug) => {
+    return (ctx) => {
+        ctx.data.faq = slug;
+        return handleFaq(ctx);
+    };
+};
