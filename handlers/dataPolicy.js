c79404b806d902466ed8c6bed4e25090896a023e
ChristianJoe <cjoe@gmx.de>
2020-03-12 17:05:16 +0100

    :children_crossing: Disable web page preview

--- handlers/dataPolicy.js
+++ handlers/dataPolicy.js
@@ -2,9 +2,9 @@ import getFaq from '../lib/faq';
 
 export default async (ctx) => {
     let faq = await getFaq('datenschutz_general');
-    await ctx.replyFullNewsBase(faq);
+    await ctx.replyFullNewsBase(faq, { 'disable_web_page_preview': true });
     faq = await getFaq('datenschutz_tracking');
-    await ctx.replyFullNewsBase(faq);
+    await ctx.replyFullNewsBase(faq, { 'disable_web_page_preview': true });
     faq = await getFaq('datenschutz_policies');
-    await ctx.replyFullNewsBase(faq);
+    await ctx.replyFullNewsBase(faq, { 'disable_web_page_preview': true });
 };
