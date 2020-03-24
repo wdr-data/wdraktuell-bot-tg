25bef5b9717c7a095f7bb0af463b1757b136ff7c
ChristianJoe <cjoe@gmx.de>
2020-03-24 17:38:12 +0100

    :recycle: Refactor location handler

--- handlers/location.js
+++ handlers/location.js
@@ -9,75 +9,83 @@ const uri = 'https://covid19nrw.netlify.com/.netlify/functions/get_nrw';
 
 export const handleLocation = async (ctx) => {
     const location = ctx.dialogflowParams.location.structValue.fields;
+    console.log(`Detected location: ${location}`);
     let messageText = ctx.dialogflowResponse;
-
-    if (location.city.stringValue) {
-        console.log(`Detected city: ${location.city.stringValue}`);
-        return handleCity(ctx, location.city.stringValue);
-    } else if (location['zip-code'].stringValue) {
-        const queryZipCode = location['zip-code'].stringValue;
-        if (byZipCodes[queryZipCode]) {
-            return handleCity(ctx, byZipCodes[queryZipCode].city, queryZipCode);
-        }
+    const zipCode = location['zip-code'].stringValue;
+    let city = location.city.stringValue;
+    console.log(`zipCode: ${zipCode}`);
+    console.log(`city: ${zipCode}`);
+    if (byZipCodes[zipCode]) {
+        city = byZipCodes[zipCode].city;
     }
-
+    if (byCities[city]) {
+        return handleCity(ctx, byCities[city]);
+    }
+    if (city) {
+        ctx.track({
+            category: 'Unterhaltung',
+            event: 'Dialogflow',
+            label: 'Location-Feature',
+            subType: city,
+        });
+    }
+    if (city || zipCode) {
+        return ctx.reply(`${
+            zipCode ? `Die Postleitzahl ${zipCode}` : city
+        } liegt wohl nicht in NRW. Versuche es mit einer PLZ, einem Ort oder einer Stadt aus NRW.`);
+    }
     return ctx.reply(messageText);
 };
 
-export const handleCity = async (ctx, queryCity, queryZipCode=null) => {
+export const handleCity = async (ctx, cityFull) => {
     const covidText = await getFaq(`locationcovidnrw`);
-    if (byCities[queryCity]) {
-        const city = byCities[queryCity];
-        const covidData = await getCovid(city.district);
 
-        const studioUrl = trackLink(byStudios[city.studio].linkCorona, {
+    const covidData = await getCovid(cityFull.district);
+
+    const studioUrl = trackLink(byStudios[cityFull.studio].linkCorona, {
+        campaignType: 'unterhaltung',
+        campaignName: `Corona Info Studio ${cityFull.studio}`,
+        campaignId: 'covid',
+    });
+    const studioLink = `\n🔗 <a href="${escapeHTML(studioUrl)}">${
+        escapeHTML(`Coronavirus in NRW - Studio ${cityFull.studio}`)
+    }</a>`;
+
+    const ddjUrl = trackLink(
+        'https://www1.wdr.de/nachrichten/themen/coronavirus/corona-daten-nrw-100.html', {
             campaignType: 'unterhaltung',
-            campaignName: `Corona Info Studio ${city.studio}`,
+            campaignName: `Zahlen Corona-Krise NRW`,
             campaignId: 'covid',
         });
-        const studioLink = `\n🔗 <a href="${escapeHTML(studioUrl)}">${
-            escapeHTML(`Coronavirus in NRW - Studio ${city.studio}`)
-        }</a>`;
-        const ddjUrl = trackLink(
-            'https://www1.wdr.de/nachrichten/themen/coronavirus/corona-daten-nrw-100.html', {
-                campaignType: 'unterhaltung',
-                campaignName: `Zahlen Corona-Krise NRW`,
-                campaignId: 'covid',
-            });
-        const ddjLink = `\n🔗 <a href="${escapeHTML(ddjUrl)}">${
-            escapeHTML(`Aktuelle Zahlen zur Corona-Krise in NRW`)
-        }</a>`;
+    const ddjLink = `\n🔗 <a href="${escapeHTML(ddjUrl)}">${
+        escapeHTML(`Aktuelle Zahlen zur Corona-Krise in NRW`)
+    }</a>`;
 
-        const messageText = `Hier die aktuellen Zahlen für ${
-            city.city
-        }${
-            city.keyCity.slice(-1) === '0' ? '' : ' im Landkreis ' + city.district
-        }:\n${covidData.infected} positiv auf Covid19 getestete Menschen. Das sind ${
-            covidData.per100k
-        } Menschen pro 100.000 Einwohner.\n\nMit ${
-            covidData.max.per100k
-        } wurde die meisten positiven Tests pro 100.000 Einwohner in ${
-            covidData.max.district
-        } registriert.\nDie wenigsten positiven Tests in NRW wurden in ${
-            covidData.min.district
-        } mit ${
-            covidData.min.per100k
-        } pro 100.000 Einwohner gezählt.\n\n(Stand: ${
-            covidData.publishedDate
-        })\n\n`;
+    const messageText = `Hier die aktuellen Zahlen für ${
+        cityFull.city
+    }${
+        cityFull.keyCity.slice(-1) === '0' ? '' : ' im Landkreis ' + cityFull.district
+    }:\n${covidData.infected} positiv auf Covid19 getestete Menschen. Das sind ${
+        covidData.per100k
+    } Menschen pro 100.000 Einwohner.\n\nMit ${
+        covidData.max.per100k
+    } wurde die meisten positiven Tests pro 100.000 Einwohner in ${
+        covidData.max.district
+    } registriert.\nDie wenigsten positiven Tests in NRW wurden in ${
+        covidData.min.district
+    } mit ${
+        covidData.min.per100k
+    } pro 100.000 Einwohner gezählt.\n\n(Stand: ${
+        covidData.publishedDate
+    })\n\n`;
 
-        return ctx.reply(
-            escapeHTML(messageText) + `${escapeHTML(covidText.text)}\n${ddjLink}\n${studioLink}`,
-            {
-                'parse_mode': 'HTML',
-                'disable_web_page_preview': true,
-            }
-        );
-    }
-
-    return ctx.reply(`${
-        queryZipCode ? `Die Postleitzahl ${queryZipCode}` : queryCity
-    } liegt wohl nicht in NRW. Versuche es mit einer PLZ, einem Ort oder einer Stadt aus NRW.`);
+    return ctx.reply(
+        escapeHTML(messageText) + `${escapeHTML(covidText.text)}\n${ddjLink}\n${studioLink}`,
+        {
+            'parse_mode': 'HTML',
+            'disable_web_page_preview': true,
+        }
+    );
 };
 
 export const getCovid = async (district) => {
