3bf7585490c6c16f4b6378eaba398e25cdad660b
ChristianJoe <cjoe@gmx.de>
2020-10-28 19:23:57 +0100

    📈  Rename campaign type to feature

--- handlers/locationSchools.js
+++ handlers/locationSchools.js
@@ -114,7 +114,7 @@ export const handleAGS = async (ctx, ags) => {
 
     const ddjUrl = trackLink(
         'https://www1.wdr.de/nachrichten/digitalisierung-schulen-umfrage-kommunen-100.html', {
-            campaignType: 'unterhaltung',
+            campaignType: 'feature',
             campaignName: `Zahlen Digitalisierung Schulen`,
             campaignId: 'feature',
         });
